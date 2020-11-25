ec964998cadcb22740c3f6fc4b64bb124590fc09
Eric Anderson <eanders@microsoft.com>
2020-11-24 17:15:27 -0800

    Update main.ts, test.ts

--- main.ts
+++ main.ts
@@ -34,3 +34,10 @@ namespace webcam {
         })
     }
 }
+
+// just paint background with image
+webcam.onFrameReceived(function() {
+    const frame = webcam.currentFrame;
+    if (frame)
+        scene.setBackgroundImage(frame)
+})
--- test.ts
+++
@@ -1,6 +0,0 @@
-// just paint background with image
-webcam.onFrameReceived(function() {
-    const frame = webcam.currentFrame;
-    if (frame)
-        scene.setBackgroundImage(frame)
-})
