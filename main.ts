2a7577e72f3d8be7b1d538f97aec49bafdcb9d2b
Rami Bououni <rami.bououni@microsoft.com>
2022-02-11 10:00:03 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -34,11 +34,3 @@ namespace webcam {
         })
     }
 }
-
-// just paint background with image
-webcam.onFrameReceived(function() {
-    const frame = webcam.currentFrame;
-    if (frame) {
-        scene.setBackgroundImage(frame)
-    }
-})
